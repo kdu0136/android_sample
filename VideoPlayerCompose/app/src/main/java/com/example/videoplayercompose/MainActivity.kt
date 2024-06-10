0874f0e3a12fb0a3b11e37740a8f25699cc8c318
kdu0136 <kdu0136@naver.com>
2024-06-10 13:58:15 +0900

    feat: add video player with exoPlayer & jetpack compose example

---
+++ VideoPlayerCompose/app/src/main/java/com/example/videoplayercompose/MainActivity.kt
@@ -0,0 +1,185 @@
+package com.example.videoplayercompose
+
+import android.app.PictureInPictureParams
+import android.app.RemoteAction
+import android.content.pm.PackageManager
+import android.graphics.RectF
+import android.os.Build
+import android.os.Bundle
+import android.util.Rational
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FileOpen
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toAndroidRect
+import androidx.compose.ui.graphics.toAndroidRectF
+import androidx.compose.ui.layout.boundsInWindow
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.graphics.toRect
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.media3.ui.PlayerView
+import com.example.videoplayercompose.ui.theme.VideoPlayerComposeTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    private val isPipSupported by lazy {
+        packageManager.hasSystemFeature(
+            PackageManager.FEATURE_PICTURE_IN_PICTURE
+        )
+    }
+
+    private var videoViewBounds = RectF()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            VideoPlayerComposeTheme {
+                val viewModel = hiltViewModel<MainViewModel>()
+                val videoItems by viewModel.videoItems.collectAsState()
+                val selectVideoLauncher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.GetContent(),
+                    onResult = { uri ->
+                        uri?.let(viewModel::addVideoUri)
+                    }
+                )
+                var lifecycle by remember {
+                    mutableStateOf(Lifecycle.Event.ON_CREATE)
+                }
+                val lifecycleOwner = LocalLifecycleOwner.current
+                DisposableEffect(key1 = lifecycleOwner) {
+                    val observer = LifecycleEventObserver { _, event -> lifecycle = event }
+                    lifecycleOwner.lifecycle.addObserver(observer)
+                    onDispose {
+                        lifecycleOwner.lifecycle.removeObserver(observer)
+                    }
+                }
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                ) {
+                    AndroidView(
+                        factory = { context ->
+                            PlayerView(context).also {
+                                it.player = viewModel.player
+                            }
+                        },
+                        update = {
+//                                 when (lifecycle) {
+//                                     Lifecycle.Event.ON_PAUSE -> {
+//                                         it.onPause()
+//                                         it.player?.pause()
+//                                     }
+//                                     Lifecycle.Event.ON_RESUME -> {
+//                                         it.onResume()
+//                                     }
+//                                     else -> Unit
+//                                 }
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .aspectRatio(16 / 9f)
+                            .onGloballyPositioned {
+                                videoViewBounds = it.boundsInWindow().toAndroidRectF()
+                            }
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    IconButton(onClick = {
+                        selectVideoLauncher.launch("video/*")
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.FileOpen,
+                            contentDescription = "Select video"
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    LazyColumn(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        items(videoItems) { item ->
+                            Text(
+                                text = item.name,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .clickable {
+                                        viewModel.playVideo(item.contentUri)
+                                    }
+                                    .padding(16.dp)
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun updatedPipParams(): PictureInPictureParams? {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            PictureInPictureParams.Builder()
+                .setSourceRectHint(videoViewBounds.toRect())
+                .setAspectRatio(Rational(16, 9))
+                .setActions(
+                    listOf(
+//                        RemoteAction(
+//                            Icon.createWithResource(
+//                                applicationContext,
+//                                R.drawable.ic_baseline_baby_changing_station_24
+//                            ),
+//                            "Baby changing station",
+//                            "Baby changing station",
+//                            PendingIntent.getBroadcast(
+//                                applicationContext,
+//                                0,
+//                                Intent(applicationContext, MyReceiver::class.java),
+//                                PendingIntent.FLAG_IMMUTABLE
+//                            )
+//                        )
+                    )
+                )
+                .build()
+        } else null
+    }
+
+    override fun onUserLeaveHint() {
+        super.onUserLeaveHint()
+        if(!isPipSupported) {
+            return
+        }
+        updatedPipParams()?.let { params ->
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                enterPictureInPictureMode(params)
+            }
+        }
+    }
+}
