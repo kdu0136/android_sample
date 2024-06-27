d7e21eebf081e7f6266db156486281173b94bc17
kdu0136 <kdu0136@naver.com>
2024-06-27 10:58:16 +0900

    feat: NavigableListDetailPaneScaffold example

---
+++ ListPaneScaffold/app/src/main/java/com/example/listpanescaffold/MainActivity.kt
@@ -0,0 +1,129 @@
+package com.example.listpanescaffold
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
+import androidx.compose.material3.adaptive.layout.AnimatedPane
+import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffoldRole
+import androidx.compose.material3.adaptive.navigation.NavigableListDetailPaneScaffold
+import androidx.compose.material3.adaptive.navigation.rememberListDetailPaneScaffoldNavigator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.listpanescaffold.ui.theme.ListPaneScaffoldTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ListPaneScaffoldTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ListDetailLayout(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3AdaptiveApi::class)
+@Composable
+fun ListDetailLayout(modifier: Modifier = Modifier) {
+    val navigator = rememberListDetailPaneScaffoldNavigator<Any>()
+    NavigableListDetailPaneScaffold(
+        modifier = modifier,
+        navigator = navigator,
+        listPane = {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentPadding = PaddingValues(16.dp)
+            ) {
+                items(100) {
+                    Text(
+                        modifier = Modifier
+                            .fillParentMaxWidth()
+                            .padding(16.dp)
+                            .clickable {
+                                navigator.navigateTo(
+                                    pane = ListDetailPaneScaffoldRole.Detail,
+                                    content = "Item $it"
+                                )
+                            },
+                        text = "Item $it"
+                    )
+                }
+            }
+        },
+        detailPane = {
+            val content = navigator.currentDestination?.content?.toString() ?: "Select an item"
+            AnimatedPane {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.primaryContainer),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Text(text = content)
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+                        AssistChip(
+                            onClick = {
+                                navigator.navigateTo(
+                                    pane = ListDetailPaneScaffoldRole.Extra,
+                                    content = "Option 1"
+                                )
+                            },
+                            label = {
+                                Text(text = "Option 1")
+                            }
+                        )
+                        AssistChip(
+                            onClick = {
+                                navigator.navigateTo(
+                                    pane = ListDetailPaneScaffoldRole.Extra,
+                                    content = "Option 2"
+                                )
+                            },
+                            label = {
+                                Text(text = "Option 2")
+                            }
+                        )
+                    }
+                }
+            }
+        },
+        extraPane = {
+            val content = navigator.currentDestination?.content?.toString() ?: "Select an item"
+            AnimatedPane {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.tertiaryContainer),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Text(text = content)
+                }
+            }
+        }
+    )
+}
